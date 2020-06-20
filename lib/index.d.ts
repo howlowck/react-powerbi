1bde300cf6f0963349d02974abbde8de9604680a
Hao Luo <howlowck@gmail.com>
2020-06-20 11:29:25 -0500

    built

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,12 @@
 import React from 'react';
 import { IEmbedConfiguration } from 'embed';
 declare type EmbedType = 'report' | 'dashboard' | 'tile';
-declare type ExtraSettings = {
-    onEmbedded?: () => void;
+interface PowerBIEmbeddedProps extends IEmbedConfiguration, React.HTMLAttributes<HTMLDivElement> {
+    onLoad?: () => void;
     embedType?: EmbedType;
     mobile?: boolean;
     filterPaneEnabled?: boolean;
     navContentPaneEnabled?: boolean;
-};
-declare type PowerBIEmbeddedProps = IEmbedConfiguration & ExtraSettings;
+}
 declare const PowerBIEmbedded: React.FC<PowerBIEmbeddedProps>;
 export default PowerBIEmbedded;
