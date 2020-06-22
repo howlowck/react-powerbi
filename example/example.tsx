4f06ed141355ae3a88a8670878a8349799688c75
Hao Luo <howlowck@gmail.com>
2020-06-21 23:12:44 -0500

    fix mobile reset error

--- example/example.tsx
+++ example/example.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import PowerBiEmbedded from '../src/index'
 
@@ -8,16 +8,19 @@ type Props = {
 
 // eslint-disable-next-line react/prop-types
 const Example: React.FC<Props> = ({token}) => {
+  const [mobileVal, setMobileVal] = useState(false)
+  const [widthValue, setWidthValue] = useState(400)
   return <div>
+    <button onClick={() => {setMobileVal(true)}}>Make Mobile</button>
+    <button onClick={() => {setMobileVal(false)}}>Remove Mobile</button>
+    <input onChange={(evt) => {setWidthValue(+evt.target.value)}} value={widthValue}/>
     <PowerBiEmbedded
       id="f6bfd646-b718-44dc-a378-b73e6b528204"
-      width={400}
-      height={300}
+      width={widthValue}
+      height={700}
       pageName="test"
-      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-      // onLoad={() => {
-      //   console.log('rendered!!!')
-      // }}
+      mobile={mobileVal}
+      // onEmbed={() => console.log('loaded!!!!!')}
       permissions={7}
       embedType="report"
       embedUrl="https://app.powerbi.com/reportEmbed?reportId=f6bfd646-b718-44dc-a378-b73e6b528204&groupId=be8908da-da25-452e-b220-163f52476cdd&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLU5PUlRILUNFTlRSQUwtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlfX0%3d"
@@ -33,7 +36,7 @@ fetch('https://powerbiplaygroundbe.azurewebsites.net/api/Reports/SampleReport')
 
     ReactDOM.render(
       <React.StrictMode>
-        <Example token={token}/>
+        <Example token={token} />
       </React.StrictMode>,
       document.getElementById('root')
     )
